a7e1d87d01adcbaecde5f6324aea7add63ce1415
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-05 23:57:48 +0700

    update learn js day 2
    day 2: learn about operator

---
+++ day_2_operator/main.js
@@ -0,0 +1,21 @@
+//Toán tử gán và toán tử số học
+var a = 5
+var b = 10
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a ** b) //lũy thừa
+console.log(a % b) //chia lấy dư
+console.log(a++)
+console.log(b--)
+
+//Toán tử so sánh
+if (a > b) {
+    alert('Số a lớn hơn b')
+} 
+
+//Toán tử login
+if (a > 0 && b > 0) {
+    alert('a và b đều lớn hơn 0')
+}

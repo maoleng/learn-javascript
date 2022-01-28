85039f07c4626a559b33cd74e1fabaf48edf9631
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-29 02:31:44 +0700

    new challenger: code by using js

--- day_13_return_in_function/homework/cau_5.js
+++
@@ -1,9 +0,0 @@
-// Viết hàm kiểm tra xem đủ tuổi để… chưa? 
-//Đặt tên hàm là checkAgeBuyAlcohol và tự nhìn Input & Expect mà giải nhé =))
-
-function checkAgeBuyAlcohol(age) {
-    if ( age >= 18 ) {
-        return true
-    }
-    return false
-}

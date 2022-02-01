b127b97f12b38b96e71a812866505ab5a0ca4fe3
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-01 11:23:48 +0700

    new lesson: loop in js

---
+++ day_18_loop/script.js
@@ -0,0 +1,111 @@
+// for ( var i = 1; i <= 1000; i++ ) {
+//     console.log(i)
+// }
+
+// ------------------------------------------------
+
+my_array = [
+    'HTML',
+    'Javascript',
+    'PHP',
+    'AutoIT',
+    'Ruby'
+]
+// var array_length = my_array.length
+
+// for ( var i = 0; i < array_length; i++ ) {
+//     console.log(my_array[i])
+// }
+
+// ------------------------------------------------
+
+// vòng lặp for/in lấy ra key của object
+
+my_info = {
+    name: 'Huu Loc',
+    age: 18,
+    role: 'student'
+}
+// for ( each in my_info ) {
+//     console.log(each)
+// }
+// for ( each in my_info ) {
+//     console.log(my_info[each])
+// }
+
+// for ( key in my_array ) {
+//     console.log(key)
+// }
+
+// my_string = "Javascript"
+// for ( key in my_string ) {
+//     console.log(my_string[key])
+// }
+
+// ------------------------------------------------
+
+// vòng for/of lấy ra phần tử mảng
+// for ( value of Object.keys(my_info) ) {
+//     console.log(my_info[value])
+// }
+
+// my_object_value = Object.values(my_info)
+// for ( value of my_object_value ) {
+//     console.log(value)
+// }
+
+// ------------------------------------------------
+
+// vòng while
+// var i = 1
+// while ( i <= 100 ) {
+//     console.log(i)
+//     i++
+// }
+
+// ------------------------------------------------
+
+// vòng do while
+// var i = 0
+// do {
+//     i++
+//     console.log(i)
+// } while ( i < 10 )
+
+// ------------------------------------------------    
+
+// break trong loop
+// var i = 1
+// for ( i = 1; i <= 10; i++ ) {
+//     if ( i > 5 ) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+// ------------------------------------------------    
+
+// continue trong loop
+// var i = 1
+// for ( i = 1; i <= 10; i++ ) {
+//     if ( i % 2 == 0 ) {
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// ------------------------------------------------    
+
+// vòng lặp lồng nhau
+// an_array = [
+//     [1, 2],
+//     [2, 3],
+//     [3, 4]
+// ]
+
+// for ( var i = 0; i < an_array.length; i++ ) {
+//     for ( var j = 0; j < an_array[i].length; j++ ) {
+//         console.log(an_array[i][j])
+//     }
+// }
+

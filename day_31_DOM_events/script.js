3b2f21e5e68f5789ceb40f57dfff7640702396d0
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-24 12:19:43 +0700

    more about DOM event

--- day_31_DOM_events/script.js
+++ day_31_DOM_events/script.js
@@ -11,8 +11,36 @@ var element_boxs = document.querySelectorAll('.box')
 
 for (var i = 0; i < element_boxs.length; i++) {
     // tham số event là hành động khi click vào
-
     element_boxs[i].onclick = function(event) {
         console.log(event.target)
     }
+}
+
+//bắt sự kiện thay đổi input text
+var text_input_element = document.querySelector('#input_text')
+text_input_element.onchange = function (event) {
+    //thuộc tính target trả về chính element đó
+    //lấy ra value của input text
+    console.log(event.target.value)
+}
+
+//bắt sự kiện Khi tick vào checkbox
+var element_check_box = document.querySelector('#input_check_box')
+element_check_box.onchange = function (event) {
+    console.log(event.target.checked)
+}
+
+//bắt sự kiện khi thay đổi selection
+var select_option = document.querySelector('select')
+select_option.onchange = function (event) {
+    console.log(event.target.value)
+}
+
+//bắt sự kiện khi người dùng ấn vào phím
+var test_keyboard = document.querySelector('#input_test_keyboard')
+test_keyboard.onkeypress = function (event) {
+    if ( event.keyCode == 13 ) {
+        console.log("may da nhan nut gui tin nhan")
+    }
+    
 }
